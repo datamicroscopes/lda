31571cccdc6fa50e3dcf064235b2c338c985626c
Tim Hopper <tdhopper@gmail.com>
2015-05-21 18:18:29 -0400

    Add empty c++ files

---
+++ test/cxx/test_state.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <microscopes/common/variadic/dataview.hpp>
+#include <microscopes/common/random_fwd.hpp>
+#include <microscopes/common/assert.hpp>
+#include <microscopes/common/util.hpp>
+#include <microscopes/lda/model.hpp>
+
+
+int main(void){
+    return 0;
+}
