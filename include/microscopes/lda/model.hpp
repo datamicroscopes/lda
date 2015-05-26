db23bfad7b852fafb50a4409f13d25f2430e9b6e
Tim Hopper <tdhopper@gmail.com>
2015-05-26 17:01:38 -0400

    Use proper LDA constructor for state object.
    And do a little bit in the test file

--- include/microscopes/lda/model.hpp
+++ include/microscopes/lda/model.hpp
@@ -26,41 +26,56 @@ namespace lda {
 
 typedef std::vector<std::shared_ptr<models::group>> group_type;
 
-class state {
-public:
-
-
-    state(const std::vector<std::shared_ptr<models::hypers>> &hypers,
-          const common::group_manager<group_type> &groups)
-        : hypers_(hypers), groups_(groups)
-    {
-        for (const auto &h : hypers_)
-            MICROSCOPES_DCHECK(h, "hyper is null");
-    }
-
-	int temp(){
-		return 1;
-	}
-
-protected:
-	std::vector<std::shared_ptr<models::hypers>> hypers_;
-	common::group_manager<group_type> groups_;
-};
 
 
 class model_definition {
 public:
-	model_definition(
-      size_t n,
-      const std::vector<std::shared_ptr<models::model>> &models)
-    : n_(n), models_(models)
-	{
-		// MICROSCOPES_DCHECK(n > 0, "no entities given");
-		// MICROSCOPES_DCHECK(models.size() > 0, "no features given");
-	}
+    model_definition(size_t n, size_t v)
+        : n_(n), v_(v)
+    {
+        MICROSCOPES_DCHECK(n > 0, "no docs");
+        MICROSCOPES_DCHECK(v > 0, "no terms");
+    }
+
+    std::vector<std::shared_ptr<models::hypers>>
+            create_hypers() const
+    {
+        std::vector<common::runtime_type> ret = std::vector<common::runtime_type>();
+    }
+
+    inline size_t n() const { return n_; }
+    inline size_t v() const { return v_; }
 private:
-	size_t n_;
-	std::vector<std::shared_ptr<models::model>> models_;
+    size_t n_;
+    size_t v_;
+};
+
+class state {
+public:
+    static const size_t MaxVocabularySize = 0x10000;
+    typedef distributions::DirichletDiscrete<MaxVocabularySize> DD;
+
+    typedef io::CRP dish_message_type;
+    typedef distributions::protobuf::DirichletDiscrete_Shared vocab_message_type;
+
+    state(const model_definition &def,
+          const common::hyperparam_bag_t &topic_init,
+          const common::hyperparam_bag_t &word_init,
+          const common::recarray::dataview &data, // should be variadic
+          const std::vector<std::vector<size_t>> &dish_assignments,
+          const std::vector<std::vector<size_t>> &table_assigments,
+          common::rng_t &rng)
+    {
+    }
+
+    // static std::shared_ptr<state>
+    // unsafe_initialize(const model_definition &def)
+    // {
+    //     return std::make_shared<state>();
+    // }
+protected:
+    std::vector<std::shared_ptr<models::hypers>> hypers_;
+    common::group_manager<group_type> groups_;
 };
 
 }
--- test/cxx/test_state.cpp
+++ test/cxx/test_state.cpp
@@ -1,14 +1,28 @@
-#include <string>
-#include <vector>
+#include <microscopes/lda/model.hpp>
+#include <microscopes/common/recarray/dataview.hpp>
+#include <microscopes/models/distributions.hpp>
+#include <microscopes/common/random_fwd.hpp>
+
+#include <random>
 #include <iostream>
 
-#include <microscopes/common/variadic/dataview.hpp>
-#include <microscopes/common/random_fwd.hpp>
-#include <microscopes/common/assert.hpp>
-#include <microscopes/common/util.hpp>
-#include <microscopes/lda/model.hpp>
-
+using namespace std;
+using namespace distributions;
+using namespace microscopes;
+using namespace microscopes::common;
+using namespace microscopes::common::recarray;
 
 int main(void){
+    const size_t D = 28*28;
+    rng_t r(5849343);
+
+    vector<shared_ptr<models::model>> models;
+    for (size_t i = 0; i < D; i++){
+        models.emplace_back(make_shared<
+            models::distributions_model<BetaBernoulli>>());
+    }
+
+    lda::model_definition def(50, 100);
+
     return 0;
 }
