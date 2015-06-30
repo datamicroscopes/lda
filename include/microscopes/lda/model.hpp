61eda269ced2cf38ac65b0da8b19a7eef468ea34
Tim Hopper <tdhopper@gmail.com>
2015-06-30 11:34:23 -0400

    Comment instance variable definitions

--- include/microscopes/lda/model.hpp
+++ include/microscopes/lda/model.hpp
@@ -579,23 +579,22 @@ public:
         }
     }
 
-    size_t V; // Vocabulary size
-    // size_t x_ji.size(); // Num documents
-    size_t m;
-    float alpha_;
-    float beta_;
-    float gamma_;
-    common::rng_t rng_;
-    std::vector<std::vector<size_t>> using_t;
-    std::vector<size_t> using_k;
-    const std::vector<std::vector<size_t>> &x_ji;
-    std::vector<std::vector<size_t>> k_jt;
-    std::vector<std::vector<size_t>> n_jt;
-    std::vector<std::vector<std::map<size_t, size_t>>> n_jtv;
-    std::vector<size_t> m_k;
-    std::vector<float> n_k;
-    std::vector<std::map<size_t, float>> n_kv;
-    std::vector<std::vector<size_t>> t_ji;
+    size_t V; // Size of vocabulary
+    size_t m; // Total number of active tables
+    float alpha_; //  Hyperparamter on second level Dirichlet process
+    float beta_; // Hyperparameter on base Dirichlet process
+    float gamma_; // Hyperparameter on first level Dirichlet process
+    common::rng_t rng_; // random number generator
+    std::vector<std::vector<size_t>> using_t; // table index (t=0 means to draw a new table)
+    std::vector<size_t> using_k; // dish(topic) index (k=0 means to draw a new dish)
+    const std::vector<std::vector<size_t>> &x_ji; // vocabulary for each document and term
+    std::vector<std::vector<size_t>> k_jt; // topics of document and table
+    std::vector<std::vector<size_t>> n_jt; // number of terms for each table of document
+    std::vector<std::vector<std::map<size_t, size_t>>> n_jtv; // number of occurrences of each term for each table of document
+    std::vector<size_t> m_k; // number of tables for each topic
+    std::vector<float> n_k; // number of terms for each topic ( + beta * V )
+    std::vector<std::map<size_t, float>> n_kv; // number of terms for each topic and vocabulary ( + beta )
+    std::vector<std::vector<size_t>> t_ji; // table for each document and term (-1 means not-assigned)
 
 
 };
