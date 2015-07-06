71d847795bfab7175cd037ada6bdf874241df436
Tim Hopper <tdhopper@gmail.com>
2015-07-06 13:56:40 -0400

    Reenable inference on run

--- microscopes/lda/runner.py
+++ microscopes/lda/runner.py
@@ -49,5 +49,4 @@ class runner(object):
         validator.validate_positive(niters, param_name='niters')
 
         for _ in xrange(niters):
-            print _
-            # self._latent.inference()
+            self._latent.inference()
