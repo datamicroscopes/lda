e17de9c153af54358d40db8284e3b78a60ae35ee
Tim Hopper <tdhopper@gmail.com>
2015-07-07 11:19:58 -0400

    Use stephens test_runner

--- microscopes/lda/testutil.py
+++ microscopes/lda/testutil.py
@@ -13,9 +13,9 @@ def toy_dataset(defn):
 
     """
 
-    lengths = 1 + np.random.poisson(lam=1.0, size=defn.n())
+    lengths = 1 + np.random.poisson(lam=1.0, size=defn.n)
     def mkrow(nwords):
-        return np.random.choice(range(defn.v()), size=nwords)
+        return np.random.choice(range(defn.v), size=nwords)
     return map(mkrow, lengths)
 
 
--- test/test_runner.py
+++ test/test_runner.py
@@ -1,13 +1,16 @@
-from microscopes.lda.definition import model_definition
 from microscopes.lda import model, runner
+from microscopes.lda.definition import model_definition
+from microscopes.lda.testutil import toy_dataset
 from microscopes.common.rng import rng
+from microscopes.common.variadic.dataview import numpy_dataview
 
 
 def test_runner_simple():
-    defn = model_definition(n=10, v=20)
-    r = rng()
-    data = [[1, 2, 3, 4, 5], [2, 3, 4]]
+    N, V = 10, 100
+    defn = model_definition(N, V)
+    data = toy_dataset(defn)
     view = data
-    latent = model.initialize(defn=defn, data=view, r=r)
-    rnr = runner.runner(defn, view, latent)
-    rnr.run(r=r, niters=1)
+    prng = rng()
+    latent = model.initialize(defn, view, prng)
+    r = runner.runner(defn, view, latent)
+    r.run(prng, 1)
