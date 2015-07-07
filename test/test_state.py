18613f787b3f36424cdfd5847d6bfac3491f9006
Tim Hopper <tdhopper@gmail.com>
2015-07-07 11:58:50 -0400

    Add stephen's test_state.py

---
+++ test/test_state.py
@@ -0,0 +1,17 @@
+from microscopes.common.rng import rng
+from microscopes.lda.definition import model_definition
+from microscopes.lda.model import initialize
+from microscopes.lda.testutil import toy_dataset
+from microscopes.common.variadic.dataview import numpy_dataview
+
+from nose.tools import assert_equals
+
+
+def test_simple():
+    N, V = 10, 100
+    defn = model_definition(N, V)
+    data = toy_dataset(defn)
+    view = data
+    prng = rng()
+    s = initialize(defn, view, prng)
+    assert_equals(s.nentities(), len(data))
