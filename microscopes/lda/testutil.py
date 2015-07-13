ea8d96219748bacc7bedbb93f5e20f3648d5748a
Tim Hopper <tdhopper@gmail.com>
2015-07-13 15:43:30 -0400

    Increase size of test docs

--- microscopes/lda/testutil.py
+++ microscopes/lda/testutil.py
@@ -13,7 +13,7 @@ def toy_dataset(defn):
 
     """
 
-    lengths = 1 + np.random.poisson(lam=1.0, size=defn.n)
+    lengths = 1 + np.random.poisson(lam=10, size=defn.n)
     def mkrow(nwords):
         return np.random.choice(range(defn.v), size=nwords)
     return map(mkrow, lengths)
