32bb799fc6a7ccccf0b2f2d8bed409bd4fb88f8d
Tim Hopper <tdhopper@gmail.com>
2015-07-14 10:08:08 -0400

    Move state constructor from state.hpp to state.cpp

--- include/microscopes/lda/model.hpp
+++ include/microscopes/lda/model.hpp
@@ -55,33 +55,8 @@ public:
           float beta,
           float gamma,
           const std::vector<std::vector<size_t>> &docs,
-          common::rng_t &rng)
-        : alpha_(alpha), beta_(beta), gamma_(gamma), x_ji(docs),
-          n_k(util::defaultdict<size_t, float>(beta * def.v())) {
-        V = def.v();
-        for (size_t i = 0; i < x_ji.size(); ++i) {
-            using_t.push_back({0});
-        }
-        dishes_ = {0};
+          common::rng_t &);
 
-        for (size_t j = 0; j < x_ji.size(); ++j) {
-            restaurants_.push_back({0});
-            n_jt.push_back({0});
-
-            n_jtv.push_back(std::vector< std::map<size_t, size_t>>());
-            for (size_t t = 0; t < using_t[j].size(); ++t)
-            {
-                n_jtv[j].push_back(std::map<size_t, size_t>());
-            }
-        }
-        m = 0;
-        m_k = std::vector<size_t> {1};
-        n_kv.push_back(util::defaultdict<size_t, float>(beta_));
-        for (size_t i = 0; i < docs.size(); i++) {
-
-            t_ji.push_back(std::vector<size_t>(docs[i].size(), 0));
-        }
-    }
 
 
     std::vector<std::vector<size_t>>
