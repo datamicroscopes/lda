5a12c1c448ed581ecac0069702f607f5e882cdb4
Tim Hopper <tdhopper@gmail.com>
2015-07-17 14:44:19 -0400

    Remove unused comment

--- microscopes/lda/definition.pyx
+++ microscopes/lda/definition.pyx
@@ -1,9 +1,5 @@
 # cython: embedsignature=True
 
-# python imports
-# from microscopes.common import validate_probability_vector
-
-
 cdef class model_definition:
     def __cinit__(self, n, v):
         self.n = n
