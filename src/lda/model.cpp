38c0ac1b9182e8aa5ec1360b96a9c6b034801a17
Tim Hopper <tdhopper@gmail.com>
2015-07-28 13:48:12 -0400

    Revert "n_kv[k].get(v) defaults to beta_ anyway"
    This reverts commit d35cf7251ffc48d6bdefdfd22f96cbaea097faba.

--- src/lda/model.cpp
+++ src/lda/model.cpp
@@ -140,7 +140,12 @@ microscopes::lda::state::word_distribution() {
         if (k == 0) continue;
         vec.push_back(std::map<size_t, float>());
         for (size_t v = 0; v < V; ++v) {
-            vec.back()[v] = n_kv[k].get(v) / n_k.get(k);
+            if (n_kv[k].contains(v)) {
+                vec.back()[v] = n_kv[k].get(v) / n_k.get(k);
+            }
+            else {
+                vec.back()[v] = beta_ / n_k.get(k);
+            }
         }
     }
     return vec;
