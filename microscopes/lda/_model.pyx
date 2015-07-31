a9a2a81c175ca090afce019d46f1365c54d07603
Tim Hopper <tdhopper@gmail.com>
2015-07-31 16:39:59 -0400

    Validate vocab length

--- microscopes/lda/_model.pyx
+++ microscopes/lda/_model.pyx
@@ -178,6 +178,8 @@ def initialize(model_definition defn, data, rng r, **kwargs):
     dish_hps : concentration parameters on base (alpha) and second-level (gamma) Dirichlet processes
     """
     numeric_docs, vocab_lookup = _initialize_data(data)
+    validator.validate_len(vocab_lookup, defn.v, "vocab_lookup")
+    if len(vocab_lookup) == defn.v
     return state(defn=defn, data=numeric_docs, vocab=vocab_lookup, r=r, **kwargs)
 
 def _initialize_data(docs):
