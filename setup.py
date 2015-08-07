194d6134f289f7b387b95cba49932a367ec4aaea
Tim Hopper <tdhopper@gmail.com>
2015-08-07 11:47:57 -0400

    Revert "Remove protobuf"
    This reverts commit ce6300e5fbf339910f572817d04bbe1199272e2b.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CYTHON_MODULES = ['microscopes.lda._model',
                   ]
 
 LIBRARY_DEPENDENCIES = ["microscopes_common", "microscopes_lda",
-                        "distributions_shared"]
+                        "protobuf", "distributions_shared"]
 
 
 def get_git_sha1():
