b21f03d32ffede7f472c8dede0028329c8c76545
Tim Hopper <tdhopper@gmail.com>
2015-08-10 17:50:20 -0400

    Add test_lda_monotone to test monotonicity of perplexity

--- test/test_reuters.py
+++ test/test_reuters.py
@@ -11,7 +11,7 @@ from microscopes.lda import utils
 
 class TestLDANewsReuters():
 
-    def setUp(self):
+    def _load_docs(self):
         test_dir = os.path.dirname(__file__)
         reuters_ldac_fn = os.path.join(test_dir, 'data', 'reuters.ldac')
         with open(reuters_ldac_fn, 'r') as f:
@@ -20,13 +20,23 @@ class TestLDANewsReuters():
         self.V = utils.num_terms(self.docs)
         self.N = len(self.docs)
 
+    def setUp(self):
+        self._load_docs()
+        self.niters = 100
+
         self.defn = model_definition(self.N, self.V)
         self.prng = rng()
         self.latent = model.initialize(self.defn, self.docs, self.prng)
         self.r = runner.runner(self.defn, self.docs, self.latent)
-        self.r.run(self.prng, 100)
+
         self.doc_topic = self.latent.document_distribution()
 
 
     def test_lda_news(self):
         assert len(self.doc_topic) == len(self.docs)
+
+    def test_lda_monotone(self):
+        # run additional iterations, verify improvement in log likelihood
+        original_perplexity = self.latent.perplexity()
+        self.r.run(self.prng, self.niters)
+        assert self.latent.perplexity() < original_perplexity
