fb812e9b53affc615f9df5dd4ee603ad150c8611
Tim Hopper <tdhopper@gmail.com>
2015-09-28 12:51:49 -0400

    Update docstrings for kernel

--- microscopes/lda/kernels.pyx
+++ microscopes/lda/kernels.pyx
@@ -1,4 +1,10 @@
 # cython: embedsignature=True
 
 def lda_crp_gibbs(state s, rng r):
+    """Gibbs transition kernel for LDA state object. Modifies
+    state object in place.
+
+    Implementation of "Posterior sampling in the Chinese restaurant
+        franchise" as described in Teh et al (2005).
+    """
     c_lda_crp_gibbs(s._thisptr.get()[0], r._thisptr[0])
