7a2a1931616488165cb187dc3bbb252eeeb33880
Tim Hopper <tdhopper@gmail.com>
2015-09-28 13:32:05 -0400

    Update docstring for model_definition

--- microscopes/lda/definition.pyx
+++ microscopes/lda/definition.pyx
@@ -2,6 +2,12 @@
 
 cdef class model_definition:
     def __cinit__(self, n, v):
+        """The LDA model structural definition
+
+        Parameters
+        ----------
+        n : Number of documents in the model
+        v :  Number of unique vocabulary words in the model
+        """
         self.n = n
         self.v = v
-        self._thisptr.reset(new c_model_definition(n, v))
