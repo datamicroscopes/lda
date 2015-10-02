7d07d7c662ede0549bedbee1c8cb61876e81eec2
Tim Hopper <tdhopper@gmail.com>
2015-10-02 17:37:03 -0400

    Wrap c++ sample_alpha and sample_gamma

--- microscopes/lda/_kernels_h.pxd
+++ microscopes/lda/_kernels_h.pxd
@@ -3,3 +3,5 @@ from microscopes.common._random_fwd_h cimport rng_t
 
 cdef extern from "microscopes/lda/kernels.hpp":
     void lda_crp_gibbs  "microscopes::kernels::lda_crp_gibbs" (state &, rng_t &)
+    void sample_gamma  "microscopes::kernels::lda_hyperparameters::sample_gamma" (state &, rng_t &, float, float)
+    void sample_alpha  "microscopes::kernels::lda_hyperparameters::sample_alpha" (state &, rng_t &, float, float)
--- microscopes/lda/kernels.pyx
+++ microscopes/lda/kernels.pyx
@@ -13,6 +13,18 @@ def lda_crp_gibbs(state s, rng r):
     """
     c_lda_crp_gibbs(s._thisptr.get()[0], r._thisptr[0])
 
+def sample_gamma(state s, rng r, float a, float b):
+    # Sample Dirichlet process disperson parameter gamma according to
+    # Gregor Heinrich's scheme seen here: http://bit.ly/1baZ3zf
+    # Follow (Escobar+West95) with n = T
+    c_sample_gamma(s._thisptr.get()[0], r._thisptr[0], a, b)
+
+def sample_alpha(state s, rng r, float a, float b):
+    # Sample Dirichlet process disperson parameter gamma according to
+    # Gregor Heinrich's scheme seen here: http://bit.ly/1baZ3zf
+    # Follow (Teh+06)
+    c_sample_alpha(s._thisptr.get()[0], r._thisptr[0], a, b)
+
 def lda_sample_dispersion(state s, rng r):
     # Sample Dirichlet process disperson parameters according to
     # Gregor Heinrich's scheme seen here: http://bit.ly/1baZ3zf
